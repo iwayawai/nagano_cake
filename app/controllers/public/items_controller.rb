77d0388138f0eca3e6b179d83d22332c18dbc46e
EC2 Default User <119268663+iwayawai@users.noreply.github.com>
2023-01-23 08:41:07 +0000

    Update genre

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,5 @@
 class Public::ItemsController < ApplicationController
+  belongs_to :genre
+  has_many :order_items, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,50 @@
 Rails.application.routes.draw do
   
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  
-  
-  
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
-  namespace :admin do
-    resources :items,:genres,:customers,:
+   scope module: :public do
+     
+  root to:"homes#top"
+  get "/about"=>"homes#about", as:"about"
+
+    resources :items,only: [:index,:show]
+    resource :customers,only: [:show,:edit,:update] do
+      collection do
+        get'unsubscribe'
+        patch 'withdraw'
+      end 
+    end 
+      
+    resources :cart_items,only: [:index,:update,:destroy,:create] do
+      collection do
+        delete'destroy_all'
+      end 
+    end 
+    
+    resources :orders,only: [:new,:create,:index,:show] do
+      collection do
+        post'confirm'
+        get'complete'
+      end 
+    end 
+    
+    resources :addresses,only: [:index,:edit,:create,:update,:destroy]
   end 
   
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+   namespace :admin do
+    root to:"homes#top"
+    resources :items
+    resources :genres,only: [:index,:create,:edit,:update]
+    resources :customers,only: [:index,:show,:edit,:update]
+    resources :orders,only: [:show,:update]
+    resources :order_details,only: [:update]
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
